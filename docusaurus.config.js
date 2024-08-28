df32e80bbc86a29d602ed04aa85477ad3c0d8650
Ingo (School Account) <0358381@schoolsnet.act.edu.au>
2024-08-28 12:29:59 +1000

    Rename site (and format thx vscode)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,11 +4,11 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'ContentGiver',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -86,7 +86,7 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          { to: '/blog', label: 'Blog', position: 'left' },
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
